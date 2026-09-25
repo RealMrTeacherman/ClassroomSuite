24f822accac901613733020b3dc0bcd15060530d
RealMrTeacherman <dabeast805@gmail.com>
2026-09-25 07:02:55 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    subfolder such as user.github.io/classroom/, or anywhere else, with no
    edits. Registering "../sw.js" from an app directory gives this worker a
    scope of the suite root, which needs no special response header. */
-const CACHE = "classroom-suite-v66-d4799d0e";
+const CACHE = "classroom-suite-v69-c5b15ee2";
 
 const SHELL = [
   "./",
